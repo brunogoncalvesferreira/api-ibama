1a54aabb8d4f6f35bf1e055366ee46e6f16c68ab
brunogoncalvesferreira <brunogoncalvesferreira@outllok.com>
2024-05-31 14:42:27 -0300

    url em produção

--- src/server.js
+++ src/server.js
@@ -9,10 +9,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
   cors({
-    origin: [
-      'http://localhost:5173',
-      'https://api-ibama-kxuh4cg6oa-uw.a.run.app',
-    ],
+    origin: ['http://localhost:5173', 'https://upload-ibama.vercel.app/'],
     credentials: true,
   }),
 )
