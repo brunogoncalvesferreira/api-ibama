41e54a87719e21ce4fa6b397e3cb5f8c4d619169
brunogoncalvesferreira <brunogoncalvesferreira@outllok.com>
2024-06-10 18:40:56 -0300

    add localhost 5173

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ app.use(cookieParser())
 app.use(
   cors({
     // origin: 'http://localhost:5173',
-    origin: 'https://upload-ibama.vercel.app',
+    origin: ['https://upload-ibama.vercel.app', 'http://localhost:5173'],
     credentials: true,
   }),
 )
